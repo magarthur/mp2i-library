6585edf9486afe555b23cb350b1e378b0b8b2691
arthur magron <89658602+magarthur@users.noreply.github.com>
2021-10-28 13:16:18 +0200

    Update list.ml

--- ds/list/list.ml
+++ ds/list/list.ml
@@ -1,3 +1,20 @@
+
+let fst_l l1 = match l1 with
+    |[] -> failwith " not elements"
+    |e::q -> e;;
+    
+
+let rec lst_l l1 = match l1 with
+    |[] -> failwith "not elements"
+    |[e] -> e
+    |e::q -> lst_l q;;
+
+
+let rec nth_l e l1 = match l1 with
+    |[] -> failwith "impossible"
+    |e1::q -> if e = 0 then e1
+                else nth_l (e-1) q;;
+
 (* [length_l l1] returns the length of [l1]*)
 let rec length_l l1 = match l1 with
     |[] -> 0
@@ -49,5 +66,65 @@ let rec decrease_l l1 = match l1 with
     |[] -> true
     |[e] -> true
     |e::e1::q -> e<e1 || increase_l (e1::q);;
+    
+(* [equal_l [a1; ...; an] [b1; ..; bm]] holds when the two input lists have the same length, and for each pair of elements ai, bi at the same position we have eq ai bi.*)
+let rec equal_l l1 l2 = match l1,l2 with
+    |[],[] -> true
+    |[],_ | _,[]-> false
+    |e::q, e1::q1 -> if e <> e1 then false 
+                    else equal_l q q1;;
+                    
+                    
+let rec map_l f l1 = match l1 with
+    | [] -> []
+    |e::q -> f(e)::map_l f q;;
 
 
+let rec filter_l f l1 = match l1 with
+    | [] -> []
+    |e::q -> if f(e) then e::filter_l f q
+            else filter_l f q;;
+
+
+let rec for_all_l f l1 = match l1 with
+    |[] -> true
+    |e::q -> if f(e) = true then for_all_l f q
+            else false;;
+            
+
+let rec exists_l f l1 = match l1 with
+    |[] -> false
+    |e::q ->  f(e) || exists_l f q
+    
+    
+let rec combine_pairs_l l1 l2 = match l1, l2 with
+    |[],[] -> []
+    |_,[] | [],_ -> failwith "Invalid_argument"
+    |e1::q1, e2::q2 -> (e1,e2)::combine_pairs_l q1 q2;;
+
+
+let rec split_pairs_l l1 = match l1 with
+    |[] -> ([],[])
+    |(e1,e2)::q -> let q1,q2 = split_pairs_l q in
+                    e1::q1, e2::q2;;
+
+
+let rec split_l l1 = match l1 with
+    |[] -> ([],[])
+    |[e] -> ([e],[])
+    |e1::e2::q -> let q1, q2 = split_l q in
+                    e1::q1, e2::q2;;
+                    
+
+let rec fusion_l l1 l2 = match l1, l2 with
+    |[],_ -> l2
+    |_,[] -> l1
+    |e1::q1, e2::q2 -> if e1< e2 then e1::fusion_l q1 l2
+                        else e2::fusion_l l1 q2;;
+                        
+
+let rec tri_l l1 = match l1 with
+    |[] -> []
+    |[e] -> [e]
+    |_ -> let l2, l3 = split_l l1 in
+        fusion_l (tri_l l2) (tri_l l3);;
