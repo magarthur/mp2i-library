d2b17941676614c678f649e3a044327bb655d4ab
arthur magron <89658602+magarthur@users.noreply.github.com>
2021-11-01 20:14:21 +0100

    Update sort_list.ml

--- algorithme/list/sort/sort_list.ml
+++ algorithme/list/sort/sort_list.ml
@@ -18,3 +18,18 @@ let rec sort_l l1 = match l1 with
     |[e] -> [e]
     |_ -> let l2, l3 = split_l l1 in
         fusion_l (sort_l l2) (sort_l l3);;
+ 
+(*partition l p returns a pair (l1, l2) where :
+- l1 is a list containing the elements of l strictly less than p
+- l2 is a list containing the elements of l greater than or equal to p *)
+let rec partition_l l1 pivot = match l with
+    |[] -> [],[]
+    |e::q -> let l2,l3 = partition q pivot in
+                if e< pivot then e::l2, l3
+                else l2, e::l3
+
+(* [quicksort_l l1] transform l1 into a sorted list *)
+let rec quicksort_l l1 = match l with
+    |[]->[]
+    |p::q -> let l2, l3 = partition q p in
+        append_l (quicksort_l l2) (quicksort_l l3)
