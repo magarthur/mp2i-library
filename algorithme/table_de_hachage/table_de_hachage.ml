da87a741e3bc8c67cc49b0fe655864d03aee84d5
arthur magron <89658602+magarthur@users.noreply.github.com>
2021-11-01 21:41:59 +0100

    Update table_de_hachage.ml

--- algorithme/table_de_hachage/table_de_hachage.ml
+++ algorithme/table_de_hachage/table_de_hachage.ml
@@ -1,7 +1,7 @@
 type ('a, 'b) table_hachage = {hache : 'a -> int; donnees : ('a*'b) list array; largeur:int};;
 
 (*create a "table_hachage" *)
-let create h w = {hache = h; donnnes = Array.make w []; largeur = w};;
+let create h w = {hache = h; donnees = Array.make w []; largeur = w};;
 
 (*[recherche t k] returns if k belongs at "table_hachage"*)
 let recherche t k =
@@ -14,21 +14,16 @@ let recherche t k =
 let element t k =
     let rec aux = function
     | [] -> failwith "not in the table"
-    |(k'e)::q -> if k=k' then e
+    |(k',e)::q -> if k=k' then e
                 else aux q in
-aux t.donnnees.(t.hache k);;
+aux t.donnees.(t.hache k);;
 
 (* [ajout t k e] add the element e in "table_hachage" with the key k *)
-let ajout t k e =
+let add t k e =
     if recherche t k == true then failwith "key already here"
-    else t.donnees.(t.hache k) <- (k,e)::t.donnnees.(t.hache k);;
+    else t.donnees.(t.hache k) <- (k,e)::t.donnees.(t.hache k);;
 
 (* [suppr t k] delete the key k in the "table_hachage"*)
-let suppr t k =
-    if recherche t k == true then failwith "key not present"
-    else let rec enlerver = function
-        |[] -> []
-        |(k',_)::q when k = k' -> enlever q
-        |e::q -> e::enlever q in
-    t.donnees.(t.hache k) <- enlever (t.donnees.(t.hache k);;
+let delete t k =
+    t.donnees.(t.hache k) <- List.filter (fun c -> fst c <> k) t.donnees.(t.hache k)
     
