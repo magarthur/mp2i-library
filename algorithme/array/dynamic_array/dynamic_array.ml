c37ff2ceda230884c0121f088a022de48c39becc
arthur magron <89658602+magarthur@users.noreply.github.com>
2021-11-05 10:17:04 +0100

    Update dynamic_array.ml

--- algorithme/array/dynamic_array/dynamic_array.ml
+++ algorithme/array/dynamic_array/dynamic_array.ml
@@ -7,9 +7,14 @@ let copy t1 t2 =
     done;;
     
     
-(*add e into d with O(1) complexity *)
+(*[add e d] add e into d with O(1) complexity *)
 let add e d =
     if d.n < Array.length d.t then (d.t.(d.n) <- e;d.n <-d.n +1) 
     else if d.n = 0 then (d.t <- [|e|]; d.n <- d.n + 1)
     else let t' = Array.make (2*d.n) d.t.(0) in
     (copy d.t t'; t'.(d.n) <- e; d.t <- t';d.n <- d.n + 1);
+    
+(* [del d] deletes the last data add in the dynamic array *)
+let del d = 
+    if d.n = 0 then failwith "array is empty"
+    else (d.n <- d.n - 1; d.t.(d.n));;
