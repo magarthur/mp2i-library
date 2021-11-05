6ed522f962f60a94fc4fdca26b2a50609d95841f
arthur magron <89658602+magarthur@users.noreply.github.com>
2021-11-05 10:50:41 +0100

    Update pile.ml

--- algorithme/pile_file/pile.ml
+++ algorithme/pile_file/pile.ml
@@ -12,6 +12,8 @@ let stack_pop_l p = match p with
   |e::q -> (e,q);;
   
   
+  
+  
 (* stack with array *)
 type 'a stack = { t: 'a array ; mutable n : int } (* n is the number of elements in the stack *)
 
@@ -25,6 +27,34 @@ let stack_push_t p e =
         p.n <- p.n +1);;
         
 (* [stack_pop_t p] retires the first element of the stack and give this element *)
-let stack_pop_t p e =
+let stack_pop_t p =
   if p.n = 0 then failwith "stack is empty"
   else (p.n <- p.n -1 ;p.t.(p.n));;
+
+
+
+
+(*stack with dynamic array*)
+type 'a dyn = {mutable t :'a array; mutable n : int};;
+
+(*copy t1 into t2 *)
+let copy t1 t2 = 
+    for i = 0 to Array.length t1 - 1 do
+    t2.(i) <- t1.(i)
+    done;;
+        
+(*add e into d with O(1) complexity *)
+let add e d =
+    if d.n < Array.length d.t then (d.t.(d.n) <- e;d.n <-d.n +1) 
+    else if d.n = 0 then (d.t <- [|e|]; d.n <- d.n + 1)
+    else let t' = Array.make (2*d.n) d.t.(0) in
+    (copy d.t t'; t'.(d.n) <- e; d.t <- t';d.n <- d.n + 1);
+
+(* [stack_push_d p e] push e in the stack p with a dynamic array*)
+let stack_push_d e p = 
+    if p.n < Array.length p.t then (p.t.(p.n) <- e; p.n <- p.n +1)
+    else if p.n = 0 then (p.t <- [|e|]; p.n <- 1)
+    else let t' = Array.make (2*p.n) p.t.(0) in
+        (copy p.t t'; t'.(p.n) <- e; p.t <- t'; p.n <- p.n +1);;
+
+(* [stack_empty_t p] and [stack_pop_t p] are the same with and without dynamic array*)
