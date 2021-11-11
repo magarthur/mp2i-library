38afd3fa4b0ea455370c4da57cca1c42ce9b5ee7
arthur magron <89658602+magarthur@users.noreply.github.com>
2021-11-12 00:02:46 +0100

    Update double_ring_buffer_circular.ml

--- algorithme/list/ring_buffer/double_ring_buffer_circular/double_ring_buffer_circular.ml
+++ algorithme/list/ring_buffer/double_ring_buffer_circular/double_ring_buffer_circular.ml
@@ -1,22 +1,21 @@
-(*écrire ce que font les questions : liste doublement chainées cyclique*)
 type 'a l2c = {elem : int; mutable prev : 'a l2c;mutable  next : 'a l2c}
 
-
+(*[create_l2c e] create a l2C with one element, e *)
 let create_l2c e =
     let rec l = {elem = e; prev = l ; next = l } in l;;
     
-      
+(* [add_l2c l e]  add e after l *)   
 let add_l2c l e =
     let l_new = {elem = e; prev = l; next = l.next } in
     l.next.prev <- l_new;
     l.next <- l_new;;
     
-    
+(* suppr_l2c l° del l *)    
 let suppr_l2c l =
     l.next.prev <- l.prev;
     l.prev.next <- l.next;;
     
-    
+(* [mem_l2c e l] returns if e belongs at l *)    
 let mem_l2c e l =
     let cur = ref l.next in
     while !cur.elem <> e && !cur != l do
@@ -24,12 +23,12 @@ let mem_l2c e l =
     done;
     !cur.elem = e;;
     
-    
+(*[length_l2c l] returns the length of l*)   
 let length_l2c l = 
     let rec aux l1 =
     if l1 == l then 1 else 1 + aux l1.next in aux l.next;;
 
-
+(* [print_l2c l] prints all elements of l*)
 let print_l2c l =
     let l_cur = ref l.next in 
     while !l_cur != l do 
@@ -39,7 +38,7 @@ let print_l2c l =
         print_int !l_cur.elem;
         print_newline ();;
         
-        
+(*[fusion_l2c l1 l2] merges [l1] and [l2] in one l2c*)        
 let fusion_l2c l1 l2 =
     (l1.next).prev <- l2;
     (l2.next).prev <- l1;
