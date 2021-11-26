9a988a9efa2e8bb22863cbdcd8e67b764f94ffcd
arthur magron <89658602+magarthur@users.noreply.github.com>
2021-11-26 13:14:28 +0100

    Update and rename algorithme/c/basic_functions/basic_functions.c to algorithme/c/array/basic_functions/basic_functions.c

---
+++ algorithme/c/array/basic_functions/basic_functions.c
@@ -0,0 +1,20 @@
+int minimum(int* t, int n){
+    int resultat = INT_MAX;
+    for(int i = 0; i<=n-1;i++){
+        if(t[i]< resultat){
+            resultat = t[i];
+        }
+    }
+    return resultat;
+}
+
+
+int maximum(int* t, int n){
+    int resultat = INT_MIN;
+    for(int i = 0; i<=n-1; i++){
+        if(t[i]>resultat){
+            resultat = t[i];
+        }
+    }
+    return resultat;
+}
--- algorithme/c/basic_functions/basic_functions.c
+++
@@ -1,9 +0,0 @@
-int minimum(int* t, int n){
-    int resultat = INT_MAX;
-    for(int i = 0; i<=n-1;i++){
-        if(t[i]< resultat){
-            resultat = t[i];
-        }
-    }
-    return resultat;
-}
