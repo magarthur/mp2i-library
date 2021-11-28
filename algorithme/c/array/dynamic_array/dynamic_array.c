fa5aac4bb35467e0a3de6576182f08edeb7592e2
arthur magron <89658602+magarthur@users.noreply.github.com>
2021-11-28 19:35:37 +0100

    Create dynamic_array.c
    Verifier erreur sur la capacité du tableau lorsqu’on ajoute un élément

---
+++ algorithme/c/array/dynamic_array/dynamic_array.c
@@ -0,0 +1,51 @@
+struct darray {
+    int* t; // tableau utilisé pour stocker les éléments
+    int size; // nombre d'éléments
+    int capacity; // taille de t
+};
+
+typedef struct darray darray;
+
+
+darray* empty() {
+    darray* d = (darray*)malloc(sizeof(darray));
+    d->t = NULL;
+    d->size = 0;
+    d->capacity = 0;
+    return d;
+}
+
+
+void print_darray(darray* d) {
+    for(int i = 0; i < d->size; i++)
+        printf("%d", d->t[i]);
+}
+
+
+void copy(int* t1, int* t2, int n) {
+    for(int i = 0; i < n; i++)
+        t2[i] = t1[i];
+}
+
+
+void add(darray* d, int e) {
+    if(d->size == d->capacity) {
+        int* tmp = d->t; // conserve a->t pour pouvoir le free ensuite
+        d->capacity *= 2;
+        d->t = (int*)malloc(sizeof(int)*d->capacity);
+        copy(tmp, d->t, d->size);
+        free(tmp);
+    }
+    d->t[d->size] = e;
+    d->size++;
+}
+
+
+int del(darray* d){
+    d->size = d->size -1;
+    return d->t[d->size];
+}
+
+
+
+
